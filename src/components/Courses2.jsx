f156811ce8653adfc3e22c7b83ad14d3f090a1a5
Muzaffar096 <muzaffar2131@gmail.comgit config --global user.emailmuzaffar2131@gmail.com>
2024-02-08 02:18:02 +0500

    last commit

--- src/components/Courses2.jsx
+++ src/components/Courses2.jsx
@@ -1,85 +1,83 @@
 import Title from "./SectionTitle";
 import { NavLink } from "react-router-dom";
 import Button from "./Button";
-import tracks from "../data/course.json"
+import tracks from "../data/course.json";
 function Courses2() {
-
-    return (
-        <div className="container mx-auto">
-            <div className="flex flex-col lg:flex-row lg:items-center">
-                <Title
-                    title="Our Courses"
-                    subTitle="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in."
-                />
-                <Button
-                    label="View All"
-                    type="button"
-                    styles="border border-[#F1F1F3] bg-[#FCFCFD] rounded-[8px] py-[14px] px-[24px] font-[500] text-[16px] text-[262626] w-[130px] lg:ms-[200px] mt-6 lg:mt-0  hover:bg-[#eaeaeb] transition ease-in-out duration-500"
-                />
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] my-[70px]">
-                {tracks.map(track => {
-                    return (
-                        <CoursesCard
-                            key={track.id}
-                            courseCard={track}
-                        />
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
-
-function CoursesCard({
-    courseCard
-}) {
-
-    const {
-        image,
-        imageAlt,
-        Duration,
-        Level,
-        Name,
-        Track,
-        Descrption,
-    } = courseCard
-
-    return (
-        <div>
-            <div className="bg-[#fff] border border-[#F1F1F3] rounded-[12px] p-[15px] md:p-[30px] course-card overflow-hidden" data-aos="zoom-in" data-aos-duration="1500">
-                <div>
-                    <div className="relative overflow-hidden rounded-[6px]">
-                        <img src={image} alt={imageAlt} className="imgScale" aria-label="lazy" decoding="async"/>
-                    </div>
-                    <div className="flex flex-col md:flex-row md:justify-between py-[30px]">
-                        <div className="flex items-center gap-3">
-                            <div className="border border-[#F1F1F3] rounded-[12px] py-[10px] px-[16px]">
-                                <span className="text-[16px] font-[400] text-[#4C4C4D]">{Duration}</span>
-                            </div>
-                            <div className="border border-[#F1F1F3] rounded-[12px] py-[10px] px-[16px]">
-                                <span className="text-[16px] font-[400] text-[#4C4C4D]">{Level}</span>
-                            </div>
-                        </div>
-                        <span className="font-[600] text-[16px] text-[#262626] pt-5 md:pt-0">{Name}</span>
-                    </div>
-                    <h4 className="font-[700] text-[20px] text-[#262626] pb-3">{Track}</h4>
-                    <div className="pb-5">
-                        <span className="font-[400] text-[14px] text-[#4C4C4D]">{Descrption}</span>
-                    </div>
-                    <div>
-                        <NavLink to="./courses">
-                            <Button
-                                label="Get it Now"
-                                type="button"
-                                styles="border border-[#F1F1F3] rounded-[8px] bg-[#F7F7F8] w-[100%] py-[12px] font-[500] text-[15px] hover:bg-[#eaeaeb] transition ease-in-out duration-500"
-                            />
-                        </NavLink>
-                    </div>
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col lg:flex-row lg:items-center">
+        <Title
+          title="Our Courses"
+          subTitle="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in."
+        />
+        <Button
+          label="View All"
+          type="button"
+          styles="border border-[#F1F1F3] bg-[#FCFCFD] rounded-[8px] py-[14px] px-[24px] font-[500] text-[16px] text-[262626] w-[130px] lg:ms-[200px] mt-6 lg:mt-0  hover:bg-[#eaeaeb] transition ease-in-out duration-500"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] my-[70px]">
+        {tracks.map((track) => {
+          return (
+            <div
+              key={track.id}
+              className="bg-[#fff] border border-[#F1F1F3] rounded-[12px] p-[15px] md:p-[30px] course-card overflow-hidden"
+              data-aos="zoom-in"
+              data-aos-duration="1500"
+            >
+              <div>
+                <div className="relative overflow-hidden rounded-[6px]">
+                  <img
+                    src={track.image}
+                    alt={track.imageAlt}
+                    className="imgScale w-full"
+                    aria-label="lazy"
+                    decoding="async"
+                  />
                 </div>
+                <div className="flex flex-col md:flex-row md:justify-between py-[30px]">
+                  <div className="flex items-center gap-3">
+                    <div className="border border-[#F1F1F3] rounded-[12px] py-[10px] px-[16px]">
+                      <span className="text-[16px] font-[400] text-[#4C4C4D]">
+                        {track.Duration}
+                      </span>
+                    </div>
+                    <div className="border border-[#F1F1F3] rounded-[12px] py-[10px] px-[16px]">
+                      <span className="text-[16px] font-[400] text-[#4C4C4D]">
+                        {track.Level}
+                      </span>
+                    </div>
+                  </div>
+                  <span className="font-[600] text-[16px] text-[#262626] pt-5 md:pt-0">
+                    {track.Name}
+                  </span>
+                </div>
+                <h4 className="font-[700] text-[20px] text-[#262626] pb-3">
+                  {track.Track}
+                </h4>
+                <div className="pb-5">
+                  <span className="font-[400] text-[14px] text-[#4C4C4D]">
+                    {track.Descrption}
+                  </span>
+                </div>
+                <div>
+                  <NavLink to="./courses">
+                    <Button
+                      label="Get it Now"
+                      type="button"
+                      styles="border border-[#F1F1F3] rounded-[8px] bg-[#F7F7F8] w-[100%] py-[12px] font-[500] text-[15px] hover:bg-[#eaeaeb] transition ease-in-out duration-500"
+                    />
+                  </NavLink>
+                </div>
+              </div>
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+
 
 export default Courses2;
--- src/components/Testmonial.jsx
+++ src/components/Testmonial.jsx
@@ -1,101 +1,64 @@
 import Button from "./Button";
 import Title from "./SectionTitle";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import testmonials from "../data/testmonial.json";
 
 function Testmonial() {
-
-    const testimonies = [
-        {
-            id: 1,
-            testimony: "The web design course provided a solid foundation for me. The instructors were knowledgeable and supportive, and the interactive learning environment was engaging. I highly recommend it!",
-            profile: "../../public/testimonials-1.png",
-            profileAlt: "profile1",
-            profileName: "Sarah L"
-        },
-        {
-            id: 2,
-            testimony: "The UI/UX design course exceeded my expectations. The instructor's expertise and practical assignments helped me improve my design skills. I feel more confident in my career now. Thank you!",
-            profile: "../../public/testimonials-2.png",
-            profileAlt: "profile2",
-            profileName: "Jason M"
-        },
-        {
-            id: 3,
-            testimony: "The mobile app development course was fantastic! The step-by-step tutorials and hands-on projects helped me grasp the concepts easily. I'm now building my own app. Great course!",
-            profile: "../../public/testimonials-3.png",
-            profileAlt: "profile3",
-            profileName: "Emily R"
-        },
-        {
-            id: 4,
-            testimony: "I enrolled in the graphic design course as a beginner, and it was the perfect starting point. The instructor's guidance and feedback improved my design abilities significantly.",
-            profile: "../../public/testimonials-4.png",
-            profileAlt: "profile4",
-            profileName: "Michael K"
-        },
-    ]
-
-    return (
-        <div className=" container mx-auto">
-            <div className="flex flex-col lg:flex-row lg:items-center" data-aos="fade-right" data-aos-duration="1200">
-                <Title
-                    title="Our Testmonials"
-                    subTitle="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in."
-                />
-                <Button
-                    label="View All"
-                    type="button"
-                    styles="border border-[#F1F1F3] bg-[#FCFCFD] rounded-[8px] py-[14px] px-[24px] font-[500] text-[16px] text-[262626] w-[130px] lg:ms-[200px] mt-6 lg:mt-0  hover:bg-[#eaeaeb] transition ease-in-out duration-500"
-                />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px] my-[70px]">
-                {testimonies.map(testimony => {
-                    return (
-                        <TestmonialCard
-                            key={testimony.id}
-                            textCard={testimony}
-                        />
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
-
-function TestmonialCard({
-    textCard
-}) {
-
-    const {testimony, profile, profileAlt, profileName,} = textCard
-
-    return (
-        <div className="bg-[#FFF] rounded-[8px] benefit-card" data-aos="fade-up" data-aos-duration="1200">
-            <div className="p-[15px] lg:p-[30px]">
-                <span className="text-[15px] text-[#4C4C4D] font-[400]">{testimony}</span>
-            </div>
-            <div className="border border-[#F1F1F3] bg-[#FCFCFD] p-[15px] lg:p-[30px] rounded-bl-[8px] rounded-br-[8px] flex justify-between items-end">
+  return (
+    <div className=" container mx-auto">
+      <div
+        className="flex flex-col lg:flex-row lg:items-center"
+        data-aos="fade-right"
+        data-aos-duration="1200"
+      >
+        <Title
+          title="Our Testmonials"
+          subTitle="Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in."
+        />
+        <Button
+          label="View All"
+          type="button"
+          styles="border border-[#F1F1F3] bg-[#FCFCFD] rounded-[8px] py-[14px] px-[24px] font-[500] text-[16px] text-[262626] w-[130px] lg:ms-[200px] mt-6 lg:mt-0  hover:bg-[#eaeaeb] transition ease-in-out duration-500"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px] my-[70px]">
+        {testmonials.map((testmony) => {
+          return (
+            <div
+              key={testmony.id}
+              className="bg-[#FFF] rounded-[8px] benefit-card"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
+              <div className="p-[15px] lg:p-[30px]">
+                <span className="text-[15px] text-[#4C4C4D] font-[400]">
+                  {testmony.testimony}
+                </span>
+              </div>
+              <div className="border border-[#F1F1F3] bg-[#FCFCFD] p-[15px] lg:p-[30px] rounded-bl-[8px] rounded-br-[8px] flex justify-between items-end">
                 <div className="flex items-center gap-3">
-                    <img src={profile} alt={profileAlt} aria-label="lazy" decoding="async"/>
-                    <span className="font-[600] text-[15px] text-[#333]">{profileName}</span>
+                  <img
+                    src={testmony.profile}
+                    alt={testmony.profileAlt}
+                    aria-label="lazy"
+                    decoding="async"
+                  />
+                  <span className="font-[600] text-[15px] text-[#333]">
+                    {testmony.profileName}
+                  </span>
                 </div>
                 <Button
-                    label="Read Full Story"
-                    type="button"
-                    styles="py-[14px] px-[18px] lg:py-[18px] lg:px-[24px] border border-[#F1F1F3] bg-[#F7F7F8] rounded-[8px] text-[15px] font-[500] text-[#262626] hover:bg-[#eaeaeb] transition ease-in-out duration-500"
+                  label="Read Full Story"
+                  type="button"
+                  styles="py-[14px] px-[18px] lg:py-[18px] lg:px-[24px] border border-[#F1F1F3] bg-[#F7F7F8] rounded-[8px] text-[15px] font-[500] text-[#262626] hover:bg-[#eaeaeb] transition ease-in-out duration-500"
                 />
+              </div>
             </div>
-        </div>
-    )
-}
-
-TestmonialCard.propTypes = {
-    textCard: PropTypes.exact({
-        testimony: PropTypes.string.isRequired,
-        profile: PropTypes.string.isRequired,
-        profileAlt: PropTypes.string.isRequired,
-        profileName: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired
-    })
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Testmonial;
